8bc23d6b232a23bcfcef6f7bbe884d108af03ba8
Honey Jayan <honeyjayan2000@gmail.com>
2022-06-18 22:44:55 -0400

    updated db URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/contact_list"
+    // "URI": "mongodb://localhost/contact_list"
+    "URI": "mongodb+srv://newUser:yzfcHXlYKB46U0Gi@cluster0.drlom.mongodb.net/contact_list?retryWrites=true&w=majority"
 }
